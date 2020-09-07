63c467154d8fa312e8747f845730bb4d65ad0291
Angelo <madhav1004@icloud.com>
2020-09-07 22:18:31 +0530

    Got it app
    some things should be up like returning not after insert because of knex

--- app.js
+++ app.js
@@ -67,16 +67,22 @@ app.post("/register", function (req, res) {
         trx.insert({
             hash: hash,
             email: email
-        }).returning('email')
+        })
+            .into('login')
+            .returning('email')
             .then(loginEmail => {
-                trx('users').insert({
-                    email: loginEmail,
-                    name: name,
-                    joined: new Date()
-                }).returning('*').then((user)=>{
-                    res.json(user[0])
-                })
-            }).then(trx.commit)
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+            })
+            .then(trx.commit)
             .catch(trx.rollback)
     })
         .catch(err => res.status(400).json('unable to register'))
